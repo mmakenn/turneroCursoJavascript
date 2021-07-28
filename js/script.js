d191d76ec745a62fba9890d2710b2d93481aa456
Mac Meldi Roch <mmeldiroch@gmail.com>
2021-07-27 22:08:37 -0300

    Se agregó la función de armar una agenda de especialidades (con respectivos cupos y tiempos de espera) según el día actual. Los datos se levantan desde un archivo json local

--- js/script.js
+++ js/script.js
@@ -1,9 +1,37 @@
 const MAX_DAY_TURN = 10;
 const TURN_TIME = 15; //en minutos
+const SCHEDULE = '/data/schedule.json';
+
+function chargeSchedule(){
+    $.getJSON(SCHEDULE, function(data, status) {
+        if (status === "success") {
+            let today = new Date();
+            let dayIndex = today.getDay();
+            
+            switch(dayIndex){
+                case 0:
+                    schedule = data.domingo;
+                case 1:
+                    schedule = data.lunes;
+                case 2:
+                    schedule = data.martes;
+                case 3:
+                    schedule = data.miercoles;
+                case 4:
+                    schedule = data.jueves;
+                case 5:
+                    schedule = data.viernes;
+                case 6:
+                    schedule = data.sabado;
+            }
+        }
+    });
+}
 
 function initializeRoom(){
     /* Inicio del día, se almacenan las variables en el localStorage para ser accedidas desde las distintas secciones. */
     localStorage.setItem('givenTurns', 0);
+    localStorage.setItem('schedule', JSON.stringify(schedule));
 
     $(".left-panel").fadeToggle(function(){
         if ($('#openBtn').html() == 'ABRIR CENTRAL TURNOS'){
@@ -16,10 +44,15 @@ function initializeRoom(){
 }
 
 function resetRoom(){
+    localStorage.removeItem('schedule');
     localStorage.removeItem('waitingRoom');
     localStorage.removeItem('todayIDs');
     localStorage.removeItem('givenTurns');
 }
+
+
+var schedule;
+chargeSchedule();
 
 var openBtn = document.getElementById('openBtn');
 openBtn.addEventListener("click", initializeRoom);
